dba9c55d2f8412c2ea6d43a052a45dd4001fdd02
liza81 <lizakalafat@gmail.com>
2024-12-03 23:12:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+# main - стартовый модуль проекта
 
+# Импорты из модуля functions
+from functions import func1, func2
+
+# Функция запуска импортированных функций
+def main():
+    # Вызов функций func1 и func2
+    func1()
+    func2()
+
+# Инициализационный скрипт
+if __name__ == '__main__':
+    main()
